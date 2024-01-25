61672ea7cdfbfdd51af05d3f8fbe1a146672a60a
rubayatkhan95 <khanrubayat27@gmail.com>
2024-01-25 16:18:36 +0600

    initial commit learning fragment

---
+++ app/src/main/java/com/example/productlist/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.productlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String [] productList = {"Apple", "Milk", "Powder", "Torch"};
+        listView = (ListView) findViewById(R.id.listViewId);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,productList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Fragment fragment;
+        if(i == 0){
+            fragment = new AppleFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentId, fragment);
+            fragmentTransaction.commit();
+        } else if (i == 1){
+            fragment = new MilkFragment();
+            //Same thing we can do like below code
+            getFragmentManager().beginTransaction().replace(R.id.fragmentId, fragment).commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/productlist/MilkFragment.java
@@ -0,0 +1,19 @@
+package com.example.productlist;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MilkFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_milk, container, false);
+    }
+}
